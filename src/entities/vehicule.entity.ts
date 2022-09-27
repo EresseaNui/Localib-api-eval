c377fed019c0915019b61da5d0bd24e7b8e37b16
EresseaNui <leteneur.roland@hotmail.fr>
2022-09-27 11:42:00 +0200

    ajoute de l'enum a l'entité vehicule

--- src/entities/vehicule.entity.ts
+++ src/entities/vehicule.entity.ts
@@ -1,12 +1,29 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+export enum VehiculeType {
+  VOITURE = 'voiture',
+  CAMION = 'camion',
+  MOTO = 'moto',
+  UTILITAIRE = 'utilitaire',
+}
+
+export enum VehiculeEtat {
+  A = 'A',
+  B = 'B',
+  C = 'C',
+  D = 'D',
+}
+
 @Entity()
 export class Vehicule {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
-  type: string;
+  @Column({
+    type: 'enum',
+    enum: VehiculeType,
+  })
+  type: VehiculeType;
 
   @Column()
   marque: string;
@@ -17,8 +34,11 @@ export class Vehicule {
   @Column()
   immatriculation: string;
 
-  @Column()
-  etat: string;
+  @Column({
+    type: 'enum',
+    enum: VehiculeEtat,
+  })
+  etat: VehiculeEtat;
 
   @Column()
   louer: boolean;
